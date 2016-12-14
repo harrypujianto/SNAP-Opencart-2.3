96d5a0de29325281020e4be1e243e8a68e2527be
Harry <harry.pujianto@veritrans.co.id>
2016-12-14 14:59:36 +0700

    initial commit

---
+++ admin/controller/extension/payment/snapinst.php
@@ -0,0 +1,166 @@
+<?php
+class ControllerExtensionPaymentSnapinst extends Controller {
+
+  private $error = array();
+
+  public function index() {
+    $this->load->language('extension/payment/snapinst');
+
+    $this->document->setTitle($this->language->get('heading_title'));
+
+    $this->load->model('setting/setting');
+    $this->load->model('localisation/order_status');
+	$this->config->get('curency');
+
+
+    if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+      $this->model_setting_setting->editSetting('snapinst', $this->request->post);
+
+      $this->session->data['success'] = $this->language->get('text_success');
+
+      $this->response->redirect($this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=payment', true));
+    }
+
+    $language_entries = array(
+
+      'heading_title',
+      'text_enabled',
+      'text_disabled',
+      'text_yes',
+      'text_live',
+      'text_successful',
+      'text_fail',
+      'text_all_zones',
+	    'text_edit',
+
+      'entry_environment',
+      'entry_server_key',
+      'entry_test',
+      'entry_total',
+      'entry_order_status',
+      'entry_geo_zone',
+      'entry_status',
+      'entry_sort_order',
+      'entry_min_txn',
+      'entry_currency_conversion',
+      'entry_client_key',
+      'entry_display_name',
+
+      'button_save',
+      'button_cancel'
+      );
+
+    foreach ($language_entries as $language_entry) {
+      $data[$language_entry] = $this->language->get($language_entry);
+    }
+
+    if (isset($this->error['warning'])) {
+      $data['error_warning'] = $this->error['warning'];
+    } else {
+      $data['error_warning'] = '';
+    }
+
+    $data['breadcrumbs'] = array();
+
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('text_home'),
+      'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], true)
+    );
+
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('text_extension'),
+      'href' => $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=payment', true)
+    );
+
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('heading_title'),
+      'href' => $this->url->link('extension/payment/snapinst', 'token=' . $this->session->data['token'], true)
+    );
+
+    $data['action'] = $this->url->link('extension/payment/snapinst', 'token=' . $this->session->data['token'], true);
+
+    $data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'].'&type=payment', true);
+
+    $inputs = array(
+      'snapinst_environment',
+      'snapinst_server_key',
+      'snapinst_geo_zone_id',
+      'snapinst_sort_order',
+      'snapinst_min_txn',
+      'snapinst_payment_type',
+      'snapinst_installment_terms',
+      'snapinst_currency_conversion',
+      'snapinst_status',
+      'snapinst_client_key',
+      'snapinst_display_name',
+      'snapinst_enabled_payments',
+      'snapinst_sanitization'
+    );
+
+    foreach ($inputs as $input) {
+      if (isset($this->request->post[$input])) {
+        $data[$input] = $this->request->post[$input];
+      } else {
+        $data[$input] = $this->config->get($input);
+      }
+    }
+
+    $this->load->model('localisation/order_status');
+
+    $data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
+
+    $this->load->model('localisation/geo_zone');
+
+    $data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
+
+    $this->template = 'extension/payment/snapinst.tpl';
+	$data['column_left'] = $this->load->controller('common/column_left');
+	$data['header'] = $this->load->controller('common/header');
+	$data['footer'] = $this->load->controller('common/footer');
+	
+	
+	if(!$this->currency->has('IDR'))
+	{
+		$data['curr'] = true;
+	}
+	else
+	{
+		$data['curr'] = false;
+	}
+	$this->response->setOutput($this->load->view('extension/payment/snapinst.tpl',$data));
+	
+  }
+
+  protected function validate() {
+
+    if (!$this->user->hasPermission('modify', 'extension/payment/snapinst')) {
+      $this->error['warning'] = $this->language->get('error_permission');
+    }
+
+    // check for empty values
+    if (!$this->request->post['snapinst_display_name']) {
+      $this->error['display_name'] = $this->language->get('error_display_name');
+    }
+
+    // version-specific validation
+    if (!$this->request->post['snapinst_server_key']) {
+      $this->error['server_key'] = $this->language->get('error_server_key');
+    }      
+    
+      // default values
+    if (!$this->request->post['snapinst_environment'])
+      $this->request->post['snapinst_environment'] = 1;
+
+
+    if (!$this->request->post['snapinst_server_key']) {
+      $this->error['server_key'] = $this->language->get('error_server_key');
+    }
+    
+    // currency conversion to IDR
+    if (!$this->request->post['snapinst_currency_conversion'] && !$this->currency->has('IDR'))
+      $this->error['currency_conversion'] = $this->language->get('error_currency_conversion');
+
+      return !$this->error;
+  }
+}
+?>
---
+++ admin/controller/extension/payment/snapio.php
@@ -0,0 +1,164 @@
+<?php
+class ControllerExtensionPaymentSnapio extends Controller {
+
+  private $error = array();
+
+  public function index() {
+    $this->load->language('extension/payment/snapio');
+
+    $this->document->setTitle($this->language->get('heading_title'));
+
+    $this->load->model('setting/setting');
+    $this->load->model('localisation/order_status');
+	$this->config->get('curency');
+
+
+    if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+      $this->model_setting_setting->editSetting('snapio', $this->request->post);
+
+      $this->session->data['success'] = $this->language->get('text_success');
+
+      $this->response->redirect($this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=payment', true));
+    }
+
+    $language_entries = array(
+
+      'heading_title',
+      'text_enabled',
+      'text_disabled',
+      'text_yes',
+      'text_live',
+      'text_successful',
+      'text_fail',
+      'text_all_zones',
+	    'text_edit',
+
+      'entry_environment',
+      'entry_server_key',
+      'entry_test',
+      'entry_total',
+      'entry_order_status',
+      'entry_geo_zone',
+      'entry_status',
+      'entry_sort_order',
+      'entry_min_txn',
+      'entry_currency_conversion',
+      'entry_client_key',
+      'entry_display_name',
+
+      'button_save',
+      'button_cancel'
+      );
+
+    foreach ($language_entries as $language_entry) {
+      $data[$language_entry] = $this->language->get($language_entry);
+    }
+
+    if (isset($this->error['warning'])) {
+      $data['error_warning'] = $this->error['warning'];
+    } else {
+      $data['error_warning'] = '';
+    }
+
+    $data['breadcrumbs'] = array();
+
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('text_home'),
+      'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], true)
+    );
+
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('text_extension'),
+      'href' => $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=payment', true)
+    );
+
+    $data['breadcrumbs'][] = array(
+      'text' => $this->language->get('heading_title'),
+      'href' => $this->url->link('extension/payment/snapio', 'token=' . $this->session->data['token'], true)
+    );
+
+    $data['action'] = $this->url->link('extension/payment/snapio', 'token=' . $this->session->data['token'], true);
+
+    $data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'].'&type=payment', true);
+
+    $inputs = array(
+      'snapio_environment',
+      'snapio_server_key',
+      'snapio_geo_zone_id',
+      'snapio_sort_order',
+      'snapio_min_txn',
+      'snapio_payment_type',
+      'snapio_installment_terms',
+      'snapio_currency_conversion',
+      'snapio_status',
+      'snapio_display_name',
+      'snapio_sanitization'
+    );
+
+    foreach ($inputs as $input) {
+      if (isset($this->request->post[$input])) {
+        $data[$input] = $this->request->post[$input];
+      } else {
+        $data[$input] = $this->config->get($input);
+      }
+    }
+
+    $this->load->model('localisation/order_status');
+
+    $data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
+
+    $this->load->model('localisation/geo_zone');
+
+    $data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
+
+    $this->template = 'extension/payment/snapio.tpl';
+	$data['column_left'] = $this->load->controller('common/column_left');
+	$data['header'] = $this->load->controller('common/header');
+	$data['footer'] = $this->load->controller('common/footer');
+	
+	
+	if(!$this->currency->has('IDR'))
+	{
+		$data['curr'] = true;
+	}
+	else
+	{
+		$data['curr'] = false;
+	}
+	$this->response->setOutput($this->load->view('extension/payment/snapio.tpl',$data));
+	
+  }
+
+  protected function validate() {
+
+    if (!$this->user->hasPermission('modify', 'extension/payment/snapio')) {
+      $this->error['warning'] = $this->language->get('error_permission');
+    }
+
+    // check for empty values
+    if (!$this->request->post['snapio_display_name']) {
+      $this->error['display_name'] = $this->language->get('error_display_name');
+    }
+
+    // version-specific validation
+    if (!$this->request->post['snapio_server_key']) {
+      $this->error['server_key'] = $this->language->get('error_server_key');
+    }      
+    
+      // default values
+    if (!$this->request->post['snapio_environment'])
+      $this->request->post['snapio_environment'] = 1;
+
+
+    if (!$this->request->post['snapio_server_key']) {
+      $this->error['server_key'] = $this->language->get('error_server_key');
+    }
+    
+    // currency conversion to IDR
+    if (!$this->request->post['snapio_currency_conversion'] && !$this->currency->has('IDR'))
+      $this->error['currency_conversion'] = $this->language->get('error_currency_conversion');
+
+      return !$this->error;
+  }
+}
+?>
